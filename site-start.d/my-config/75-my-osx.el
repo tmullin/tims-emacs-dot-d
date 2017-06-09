79ad6a4f65872f65691aa32ea8a896f3687f5e16
Tim Mullin <tmullin@toasttab.com>
2017-06-09 13:51:08 -0400

    Initial config for OSX.

---
+++ site-start.d/my-config/75-my-osx.el
@@ -0,0 +1,7 @@
+(when (string-equal system-type "darwin")
+  (menu-bar-mode t)
+  (tool-bar-mode 0)
+  (set-face-attribute 'default nil :font "Menlo-24")
+  (delete-other-windows)
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))
+  )
