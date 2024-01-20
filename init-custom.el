7a3361f3b99f6e81868d9dc422736772a3d95fe5
Tim Mullin <tmullin@gmail.com>
2024-01-20 19:47:07 +0000

    Update various packages

--- init-custom.el
+++ init-custom.el
@@ -33,7 +33,7 @@
  '(p4-executable "/home/tmullin/bin/p4")
  '(p4-follow-symlinks t)
  '(package-selected-packages
-   '(yasnippet-snippets eterm-256color xterm-color magit-gh-pulls smex magit dash zenburn-theme yasnippet-bundle yasnippet yaml-mode unfill sqlplus smart-mode-line rvm robe rainbow-delimiters pod-mode plsense-direx php-mode perspective perl-completion p4 multiple-cursors mmm-mode js3-mode imenu-anywhere icicles flymake-ruby flycheck-color-mode-line auto-install anything ace-jump-mode))
+   '(gh yasnippet-snippets eterm-256color xterm-color magit-gh-pulls smex magit zenburn-theme yasnippet-bundle yaml-mode unfill sqlplus smart-mode-line rvm robe rainbow-delimiters pod-mode plsense-direx php-mode perspective perl-completion p4 multiple-cursors mmm-mode js3-mode imenu-anywhere icicles flymake-ruby flycheck-color-mode-line auto-install anything ace-jump-mode))
  '(paren-mode 'paren nil (paren))
  '(show-paren-mode t)
  '(sml/no-confirm-load-theme t)
